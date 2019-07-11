45f7a1fc67fee06906e49c5c88be115b10c0e30e
kbarnes775 <kylebarnes7@gmail.com>
2019-07-11 15:32:50 -0700

    Connected MongoDB Database

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB: 'mongodb+srv://admin:webdesigns@cluster0-ffc01.mongodb.net/test?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://admin:avwebdesigns@cluster0-nz3hh.mongodb.net/test?retryWrites=true&w=majority'
 }
+
