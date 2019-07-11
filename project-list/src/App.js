3b3a39ad5c526a636e712dd97b63a5b485d7769a
Harwood97 <teh2597@gmail.com>
2019-07-11 16:12:23 -0700

    start setup

--- project-list/src/App.js
+++ project-list/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import Main from './components/Main/Main';
 
 function App() {
   return (
-
+      <div>
+          <Main />
+      </div>
   );
 }
-
 export default App;
