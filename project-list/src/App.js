d9d4e993b0c9a3f04ecde966c6b1fd535595cbae
kbarnes775 <kylebarnes7@gmail.com>
2019-07-11 17:03:00 -0700

    Added Components
    Added Register and Login Components as well as the Redux store.

--- project-list/src/App.js
+++ project-list/src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Main from './components/Main/Main';
+import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authentication';
 
-function App() {
-  return (
-      <div>
-          <Main />
-      </div>
-  );
+import Main from './components/Main/Main'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login'
+  }
 }
+
+class App extends Component {
+  render() {
+    return (
+        <Provider store = { store }>
+          <Router>
+            <div>
+              <Route exact path="/" component={ Main } />
+              {/*<Route path="/webspot" component={ WebspotMain }/>*/}
+            </div>
+          </Router>
+        </Provider>
+    );
+  }
+}
+
 export default App;
